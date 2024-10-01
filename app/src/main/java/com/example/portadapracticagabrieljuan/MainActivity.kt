e7c9945518a53e7457a80991cb92b65f727eb583
Galex <galexu@gmail.com>
2024-10-01 16:14:41 +0200

    PUNTO

--- app/src/main/java/com/example/portadapracticagabrieljuan/MainActivity.kt
+++ app/src/main/java/com/example/portadapracticagabrieljuan/MainActivity.kt
@@ -5,28 +5,29 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -85,12 +86,10 @@ fun PortadaTexto(titulo: String, autor: String, comentario1: String, comentario2
             .padding(16.dp)
     ) {
         Column(
-            verticalArrangement = Arrangement.Top,
-            modifier = Modifier.fillMaxWidth()
+            verticalArrangement = Arrangement.Top, modifier = Modifier.fillMaxWidth()
         ) {
             Text(
-                modifier = Modifier
-                    .fillMaxWidth(),
+                modifier = Modifier.fillMaxWidth(),
                 color = Color.White,
                 text = titulo,
                 fontSize = 60.sp,
@@ -99,8 +98,7 @@ fun PortadaTexto(titulo: String, autor: String, comentario1: String, comentario2
                 textAlign = TextAlign.Center
             )
             Text(
-                modifier = Modifier
-                    .fillMaxWidth(),
+                modifier = Modifier.fillMaxWidth(),
                 color = Color.White,
                 text = autor,
                 fontSize = 25.sp,
@@ -109,8 +107,7 @@ fun PortadaTexto(titulo: String, autor: String, comentario1: String, comentario2
             )
         }
         Row(
-            horizontalArrangement = Arrangement.SpaceBetween,
-            modifier = Modifier.fillMaxWidth()
+            horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth()
         ) {
             Text(
                 modifier = Modifier.weight(1f),
@@ -131,7 +128,12 @@ fun PortadaTexto(titulo: String, autor: String, comentario1: String, comentario2
                 color = Color.White,
                 text = comentario2,
                 fontSize = 20.sp,
-                textAlign = TextAlign.Center
+                textAlign = TextAlign.Center,
+                style = TextStyle(shadow = Shadow(
+                    color = Color.Black,
+                    offset = Offset(8f, 8f),
+                    blurRadius = 2f
+                ))
             )
         }
     }
